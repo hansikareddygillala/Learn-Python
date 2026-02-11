b0cecf7f40a7eedf742582fcefaee98d6e4cdbcb
hansikareddygillala <hansikagillala1111@gmail.com>
2026-02-11 22:13:05 +0530

    11/2/2026 10:12 PM

--- Progs/c58_BirthStatistics.py
+++ Progs/c58_BirthStatistics.py
@@ -1,3 +1,4 @@
+'''
 xs = []
 ys = []
 
@@ -18,3 +19,56 @@ print(ys)
 
 # You Better Watch the Video cuz I don't have that file here I can't run the code
 #But I understood it.
+
+
+cart_prices = [20, 3.5, 6.49, 8.99, 9.99, 14.98]
+
+
+def list_sum(l):
+    total_price = 0
+    for price in l:
+        total_price += price
+    print(total_price)
+
+
+list_sum(cart_prices)
+
+
+
+def prices_list(name, price):
+    l =[]
+    for i in range(0,11):
+        multiprice = price*i
+        l.append(str(i) + " x " + name + ": " + str(multiprice))
+    return l
+
+print(prices_list("Cookie", 0.79))
+'''
+
+shelf = ["Magic Mirror", "empty", "Cookie", "Book with Magic Tricks", "empty"]
+
+def add_shelf(article):
+    # Here comes your code
+    # You can access the variable "shelf" directly from within the function
+    # This does not have to be passed as a parameter, because it exists
+    # outside of the function already.
+    print("Here's where your code goes")
+
+add_shelf("Rubik's Cube")
+print(shelf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
